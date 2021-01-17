df35b40898d2386966392da90b1bebd2c4682ea4
AbuZarrIT <Abudaudqirim@icloud.com>
2021-01-17 21:18:38 +0200

    stshop

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+// let message = "5";
+// alert (message);
+// const myBirthday = '18.04.1982';
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//     showSlides((slideIndex += n));
+// }
+
+// function currentSlide(n) {
+//     showSlides((slideIndex = n));
+// }
+
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     var dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) {
+//         slideIndex = 1;
+//     }
+//     if (n < 1) {
+//         slideIndex = slides.length;
+//     }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+//     dots[slideIndex - 1].className += " active";
+// }
+// let admin = '56';
+// let name = admin;
+// alert (name);
+// let world;
+// let Username;
+// let isBoss = confirm("Ты здесь главный?");
+
+// alert( isBoss ); // true, если нажата OK
+// let name = prompt("Ваше имя?", "");
+// alert("привет " + name);
